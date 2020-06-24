0818e0fcf192290210998f3a63b54d267b0e6401
Anjali Singh <muskan.anjali.singh@gmail.com>
2020-06-24 14:47:55 +0530

    Adding the winning condition

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,34 +1,45 @@
 package com.example.tictactoe;
 
-import androidx.annotation.NonNull;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.ActionMode;
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    int active = 0; //0=circle , 1= cross
+    //We need to set some memory to store the actions
+    int[] gameState ={2,2,2,2,2,2,2,2,2};   //2 means unplayed
+    int[][] winningLocation={{0,1,2},{3,4,5},{6,7,8},   //horizontal
+            {0,3,6},{1,4,7},{2,5,8},    //vertical
+            {0,4,8},{2,4,6}};       //diagonal
+    public void dropin(View view) {
+        ImageView counter = (ImageView) view; // We dont have to specify id, because it will act on any element clicked on
+        int tapped = Integer.parseInt(counter.getTag().toString());
+        if (gameState[tapped] == 2) //Setting unset variable
+        {
+            gameState[tapped] =active;
+            counter.setTranslationY(-1000f);
+            if (active == 0) {
+                counter.setImageResource(R.drawable.donut);
+                active = 1;
+            }
+            else {
+                counter.setImageResource(R.drawable.closesma);
+                active = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(720).setDuration(300);
+            for(int[] win:winningLocation)
+            {
+                if(gameState[win[0]]==gameState[win[1]]
+                 && gameState[win[1]] ==gameState[win[2]] && gameState[win[0]]!=2)
+                {
+                    System.out.println(gameState[win[0]]);  //prints who won
+                }
+            }
 
-    public void dropin(View view)
-    {
-        ImageView counter=(ImageView) view; // We dont have to specify id, because it will act on any element clicked on
-        counter.setTranslationY(-1000f);
-        counter.setImageResource(R.drawable.numbersm);
-        counter.animate().translationYBy(1000f).rotation(720).setDuration(300);
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
